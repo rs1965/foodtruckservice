a2e2d5c945a3fd80a57280d7901d34630345368b
VegiSaiTeja <vegi.saiteja.1999@gmail.com>
2024-05-16 00:32:53 +0530

    Added cross-origin & changed haversine formula to miles

--- webApp/src/main/java/com/radient/ftsapp/service/FoodTruckService.java
+++ webApp/src/main/java/com/radient/ftsapp/service/FoodTruckService.java
@@ -1,21 +1,28 @@
 package com.radient.ftsapp.service;
+
 import com.radient.ftsapp.model.FoodTruck;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class FoodTruckService {
     private static final String API_URL = "https://data.sfgov.org/resource/rqzj-sfat.json";
 
-    @Autowired
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
+
+    public FoodTruckService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public List<FoodTruck> fetchFoodTrucks() {
         FoodTruck[] foodTrucks = restTemplate.getForObject(API_URL, FoodTruck[].class);
-        return Arrays.asList(foodTrucks);
+        assert foodTrucks != null;
+        return Arrays.stream(foodTrucks)
+                .filter(foodTruck -> !"expired".equalsIgnoreCase(foodTruck.getStatus()))
+                .collect(Collectors.toList());
     }
 }
